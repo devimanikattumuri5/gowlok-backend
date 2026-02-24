d50bdca53d5262f7353859db850f664dc97c113c
devimanikattumuri5 <devimanikattumuri@gmail.com>
2026-02-24 12:19:25 +0530

    Updated backend fixes and new changes

--- models/Admin.js
+++ models/Admin.js
@@ -1,32 +1,15 @@
-const express = require("express");
-const router = express.Router();
-const Admin = require("../models/Admin");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 
-const JWT_SECRET = "GOWLOK_SECRET_KEY";
-
-// ADMIN LOGIN
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-
-  const admin = await Admin.findOne({ email });
-  if (!admin) {
-    return res.status(400).json({ message: "Admin not found" });
-  }
-
-  const isMatch = await bcrypt.compare(password, admin.password);
-  if (!isMatch) {
-    return res.status(400).json({ message: "Wrong password" });
-  }
-
-  const token = jwt.sign(
-    { id: admin._id, role: "admin" },
-    JWT_SECRET,
-    { expiresIn: "1d" }
-  );
-
-  res.json({ token });
+const AdminSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-module.exports = router;
+module.exports = mongoose.model("Admin", AdminSchema);
