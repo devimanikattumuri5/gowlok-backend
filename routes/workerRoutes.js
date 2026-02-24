8413b5affe2501a26814d48071ce2741245e5251
devimanikattumuri5 <devimanikattumuri@gmail.com>
2026-02-24 22:39:35 +0530

    Fixed pending workers and approve route

--- routes/workerRoutes.js
+++ routes/workerRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
-const Worker = require("../models/Worker");
+const Worker = require("../models/worker");
 
 
 // ================= REGISTER WORKER =================
